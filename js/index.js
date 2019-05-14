15000deb902f94cc0d36307d7e4b30d49833a22c
Haja-andri <haja.andri@gmail.com>
2019-05-14 13:59:48 +0200

    added listner to change the nav anchor font with mouseover/mouseleave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+// Mouseover listner on the nav list
+
+// get the nav list and chage the font size/weight when the mouse 
+// go over back to normal when it leaves
+
+const nav = document.querySelectorAll('nav a');
+nav.forEach(anchor =>{
+    anchor.addEventListener('mouseover', () =>{
+        anchor.style.fontSize = '2rem';
+        anchor.style.fontweight = 'bold';
+    })
+    anchor.addEventListener('mouseleave', () =>{
+        anchor.style.fontSize = '1.6rem';
+        anchor.style.fontweight = 'normal';
+    })
+});
+console.log(nav);
